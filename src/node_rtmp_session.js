f562204bf43f2dbec3862cc4de5b0fadef424ce7
dnservice <46596156+dnservice@users.noreply.github.com>
2023-10-27 14:32:27 +0200

    merged

--- src/node_rtmp_session.js
+++ src/node_rtmp_session.js
@@ -1129,9 +1129,12 @@ class NodeRtmpSession {
     }
 
     if (context.publishers.has(this.publishStreamPath)) {
-      
-      console.log(context.publishers)
-      this.reject();
+      let publisherSession = this.sessions.get(
+        this.publishers.get(publishStreamPath)
+      );
+      publisherSession.stop();
+
+      //this.reject();
       Logger.log(`[rtmp publish] Already has a stream. id=${this.id} streamPath=${this.publishStreamPath} streamId=${this.publishStreamId}`);
       this.sendStatusMessage(this.publishStreamId, 'error', 'NetStream.Publish.BadName', 'Stream already publishing');
     } else if (this.isPublishing) {
